4c21a33774cda4cec4081dc6338ef5bd6e732c05
glowbigger <a1ovesupreme@protonmail.com>
2023-05-21 23:48:22 -0600

    test

---
+++ Scanner.ts
@@ -0,0 +1,286 @@
+// TODO source.substring(start, current) should be a function
+
+import { LangError } from './LangError';
+import { Token } from './Token';
+import { TokenType as TT } from './TokenType';
+
+// nullable helper type
+type Nullable<T>  = T | null
+type Primitive = number | string | boolean
+
+export class Scanner {
+
+  // the source input string, which will be assigned in the constructor
+  #source:string;
+
+  // the list of tokens which will be returned by scan()
+  #tokens:Array<Token> = [];
+
+  // the list of Errors encountered which will also be returned by scan()
+  #errors:Array<LangError> = [];
+
+  // these variables keep track of the position of the scanner
+  // start is the starting character of the current lexeme
+  // current is the current UNCONSUMED character
+  // line is the current line being processed (for error reporting)
+  #start:number = 0; #current:number = 0; #line:number = 1;
+
+  // a helper lookup object which stores the identifiers which
+  // are reserved keywords
+  #keywords:{ [key: string]:TT } = {
+    'and':    TT.AND,
+    'class':  TT.CLASS,
+    'else':   TT.ELSE,
+    'false':  TT.FALSE,
+    'for':    TT.FOR,
+    'fun':    TT.FUN,
+    'if':     TT.IF,
+    'nil':    TT.NIL,
+    'or':     TT.OR,
+    'print':  TT.PRINT,
+    'return': TT.RETURN,
+    'super':  TT.SUPER,
+    'this':   TT.THIS,
+    'true':   TT.TRUE,
+    'var':    TT.VAR,
+    'while':  TT.WHILE,
+  }
+
+  constructor(source:string) {
+    this.#source = source;
+    this.#keywords.and;
+  }
+
+	/*
+	 * MAIN METHOD
+   * (the only public method)
+	 */
+
+  // goes through the source string, adding tokens until the end of the
+  // string is reached, then add an EOF token to make parsing easier
+  scan():{ tokens: Array<Token>, errors: Array<LangError> } {
+    // scan all the tokens
+		while (!this.#isAtEnd()) {
+			// we finished processing the last lexeme, so move the start
+      // and then scan the next token
+			this.#start = this.#current;
+			this.#scanToken();
+		}
+
+    // now push the EOF and return the tokens
+    const endOfFileToken = new Token(TT.EOF, '', null, this.#line);
+    this.#tokens.push(endOfFileToken);
+    return { tokens:this.#tokens, errors:this.#errors};
+  }
+
+  #scanToken():void {
+		const char = this.#advance();
+		// TODO why use a break?
+		// hTTps://stackoverflow.com/questions/252489/why-was-the-switch-statement-designed-to-need-a-break
+		switch (char) {
+			/* 	SINGLE CHARACTER TOKENS:
+			 *  NOTE these have no (null) literals
+			 */
+			case '(': this.#addToken(TT.LEFT_PAREN); break;
+			case ')': this.#addToken(TT.RIGHT_PAREN); break;
+			case '{': this.#addToken(TT.LEFT_BRACE); break;
+			case '}': this.#addToken(TT.RIGHT_BRACE); break;
+			case ',': this.#addToken(TT.COMMA); break;
+			case '.': this.#addToken(TT.DOT); break;
+			case '-': this.#addToken(TT.MINUS); break;
+			case '+': this.#addToken(TT.PLUS); break;
+			case ';': this.#addToken(TT.SEMICOLON); break;
+			case '*': this.#addToken(TT.STAR); break; 
+
+			/* 	POTENTIAL TWO CHARACTER TOKENS:
+			 *  if the first character matches, check the second character
+			 *  using match(), and add the appropriate token
+			 */
+			case '!':
+				this.#addToken(this.#match('=') ? TT.BANG_EQUAL : TT.BANG);
+				break;
+			case '=':
+				this.#addToken(this.#match('=') ? TT.EQUAL_EQUAL : TT.EQUAL);
+				break;
+			case '<':
+				this.#addToken(this.#match('=') ? TT.LESS_EQUAL : TT.LESS);
+				break;
+			case '>':
+				this.#addToken(this.#match('=') ? TT.GREATER_EQUAL : TT.GREATER);
+				break;
+
+			/* 	SINGLE-LINE COMMENTS AND DIVISION:
+			 *  consume all character in the current line
+			 */
+			case '/':
+        // if a second / is found, ignore the line because it's a comment
+        // otherwise, it's a division symbol
+				if (this.#match('/')) {
+				  // A comment goes until the end of the line.
+				  while (this.#peek() != '\n' && !this.#isAtEnd()) this.#advance();
+				} else {
+				  this.#addToken(TT.SLASH);
+				}
+				break;
+
+			// WHITESPACE AND NEW LINES:
+			case ' ': 	break;
+			case '\r': 	break;
+			case '\t': 	break;
+			case '\n':
+				this.#line++;
+				break;
+
+			// STRINGS
+			case '"': 	this.#string(); break;
+
+			// NOTE we continue scanning after catching an invalid character
+			default: 
+        if (this.#isDigit(char)) {
+          this.#number();
+        } else if (this.#isAlpha(char)) {
+          this.#identifier();
+        } else {
+          this.#errors.push(
+            new LangError('Unexpected character.', '', this.#line)
+          );
+        }
+        break;
+		}
+  }
+
+	/*
+	 * HELPER FUNCTIONS
+	 */
+
+	#isAtEnd():boolean {
+		return this.#current >= this.#source.length;
+	}
+
+  // returns the current unconsumed character and advances the pointer
+	// (like peek, but advances the pointer)
+  // NOTE this is always called before checking for isAtEnd() !!!
+	#advance():string {
+		return this.#source.charAt(this.#current++);
+	}
+
+	// returns the current unconsumed character
+	// (like peek, but without advancing the pointer)
+	#peek():string {
+		if (this.#isAtEnd()) return '\0';
+	    return this.#source.charAt(this.#current);
+	}
+
+  // returns the next character after current
+  #peekNext():string {
+    if (this.#current + 1 >= this.#source.length) return '\0';
+    return this.#source.charAt(this.#current + 1);
+  }
+
+	// add a token, if no literal is given, it is null
+	#addToken(type:TT, literal:Nullable<Primitive> = null):void {
+		const lexeme = this.#source.substring(this.#start, this.#current);
+		this.#tokens.push(new Token(type, lexeme, literal, this.#line));
+	}
+
+	// conditionally advance if the current character matches the given one
+	#match(expected:string):boolean {
+		if (this.#isAtEnd()) return false;
+		if (this.#source.charAt(this.#current) != expected) return false;
+
+		this.#current++;
+		return true;
+	}
+
+  #isDigit(c:string):boolean {
+    return c >= '0' && c <= '9';
+  }
+
+  // NOTE _ is considered alpha?
+  #isAlpha(c:string):boolean {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  // so that identifiers can have numbers, but cannot start with numbers
+  #isAlphaNumeric(c:string):boolean {
+    return this.#isAlpha(c) || this.#isDigit(c);
+  }
+
+	/*
+	 * TOKENIZE STRINGS, NUMBERS, AND IDENTIFIERS
+	 */
+
+	#string():void {
+    // build the lexeme until a " is reached or the file ends
+	  while (this.#peek() != '"' && !this.#isAtEnd()) {
+      if (this.#peek() == '\n') {
+        this.#line++;
+      }
+      this.#advance();
+    }
+
+    // if the file ends, throw and error and break
+    if (this.#isAtEnd()) {
+      this.#errors.push(
+        new LangError('Unterminated string', '', this.#line)
+      );
+      return;
+    }
+
+    // otherwise the closing " has been reached, so consume it
+    this.#advance();
+
+    // trim surrounding quotes and create the token
+    // NOTE the lexeme will include " but the literal will not
+    // NOTE type String is used over string here, because String
+    // is an object
+    const value:string =
+        this.#source.substring(this.#start + 1, this.#current - 1);
+    this.#addToken(TT.STRING, value);
+	}
+
+	#number():void {
+    // unlike string(), we don't care about eof
+    // just run a pass consuming all digits, check for a .,
+    // then run a second pass consuming all digits again
+    while (this.#isDigit(this.#peek())) {
+      this.#advance();
+    }
+
+    // look for the fractional part
+    // NOTE without isDigit(peekNext()), trailing '.'s would be supported
+    // which we don't want
+    if (this.#peek() == '.' && this.#isDigit(this.#peekNext())) {
+      // Consume the .
+      this.#advance();
+
+      // consume digits
+      while (this.#isDigit(this.#peek())) {
+        this.#advance();
+      }
+    }
+
+    // we use js's float parser here to convert the scanned lexeme
+    // into the number literal
+    const value:string = 
+      this.#source.substring(this.#start, this.#current);
+    this.#addToken(TT.NUMBER, parseFloat(value));
+	}
+
+  #identifier():void {
+    // extract the identifier, it will be only letters and numbers
+    // NOTE since this is triggered by an isAlpha check, the identifier
+    // is guaranteed to start with a letter
+    while (this.#isAlphaNumeric(this.#peek())) {
+      this.#advance();
+    }
+    const text:string = this.#source.substring(this.#start, this.#current);
+
+    // check if the lexeme is a keyword, if it is not, it is an IDENTIFIER
+    let type:TT = this.#keywords[text];
+    if (type == null) type = TT.IDENTIFIER;
+    this.#addToken(type);
+  }
+}
---
+++ Token.ts
@@ -0,0 +1,28 @@
+import { TokenType } from "./TokenType";
+
+type Nullable<T> = T | null
+type Primitive = number | string | boolean
+
+export class Token {
+  type:TokenType; lexeme:string; literal: Nullable<Primitive>; line:number;
+  
+  // the type is an enum, and can be a value like CLASS or FUNCTION
+  // the lexeme is the substring the token was extracted from
+  // the literal is the actual evaluation of the lexeme as js/ts object
+  // the line is the line number the token was found in
+  // NOTE most tokens will not have a literal, so null is an option
+  constructor(type:TokenType, lexeme:string,
+              literal:Nullable<Primitive>, line:number) {
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line = line;
+  }
+  
+  toString() {
+    // by default, the string of an enum is its indexing number
+    const tokenTypeString = TokenType[this.type];
+
+    return tokenTypeString + " " + this.lexeme + " " + this.literal;
+  }
+}
