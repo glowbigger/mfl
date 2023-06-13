febcb40a8f6dedf5ff75c3e6c47add36a12e694e
yechan <yechan.han@du.edu>
2023-06-13 00:15:54 -0600

    fixed inconsistent type hinting styling

--- astPrinter.ts
+++ astPrinter.ts
@@ -6,32 +6,32 @@ export default class AstPrinter implements expr.ExprVisitor<string> {
   // one of the below methods based on their type
   // NOTE this method is not needed, you can alternatively pass
   // a particular AstPrinter object to an Expr object
-  print(expr:Nullable<expr.Expr>):string {
+  print(expr: Nullable<expr.Expr>): string {
     if (expr !== null) {
       return expr.accept(this);
     }
     return "AstPrinter received a null";
   }
 
-  visitBinaryExpr(expr:expr.Binary):string {
+  visitBinaryExpr(expr: expr.Binary): string {
     return this.parenthesize(expr.operator.lexeme, expr.left, expr.right);
   }
 
-  visitGroupingExpr(expr:expr.Grouping):string {
+  visitGroupingExpr(expr: expr.Grouping): string {
     return this.parenthesize("group", expr.expression);
   }
 
-  visitLiteralExpr(expr:expr.Literal):string {
+  visitLiteralExpr(expr: expr.Literal): string {
     if (expr.value == null) return "nil";
     return expr.value.toString();
   }
 
-  visitUnaryExpr(expr:expr.Unary):string {
+  visitUnaryExpr(expr: expr.Unary): string {
     return this.parenthesize(expr.operator.lexeme, expr.right);
   }
 
   // wraps ( ) around a name and a list of subexpressions
-  private parenthesize(name:string, ...exprs:expr.Expr[]):string {
+  private parenthesize(name: string, ...exprs: expr.Expr[]): string {
     let outputString = "";
 
     outputString += "(" + name; 
--- token.ts
+++ token.ts
@@ -2,15 +2,15 @@ import { TokenType } from "./tokenType";
 import { Nullable, Primitive } from "./types";
 
 export default class Token {
-  type:TokenType; lexeme:string; literal: Nullable<Primitive>; line:number;
+  type: TokenType; lexeme: string; literal: Nullable<Primitive>; line: number;
   
   // the type is an enum, and can be a value like CLASS or FUNCTION
   // the lexeme is the substring the token was extracted from
   // the literal is the actual evaluation of the lexeme as js/ts object
   // the line is the line number the token was found in
   // NOTE most tokens will not have a literal, so null is an option
-  constructor(type:TokenType, lexeme:string,
-              literal:Nullable<Primitive>, line:number) {
+  constructor(type: TokenType, lexeme: string,
+              literal: Nullable<Primitive>, line: number) {
 		this.type = type;
 		this.lexeme = lexeme;
 		this.literal = literal;
